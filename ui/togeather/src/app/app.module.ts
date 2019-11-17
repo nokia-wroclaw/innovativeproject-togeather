4884e5e963592366478dac8b2405b25c789e11a0
Mateusz Szalowski <298976@uwr.edu.pl>
2019-11-17 00:51:10 +0000

    UI: Create header component.

--- ui/togeather/src/app/app.module.ts
+++ ui/togeather/src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule, MatMenuModule } from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatIconModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
